9c927083770c6212da079398a26adef3f2aad5bf
Nick <32829@ma-web.nl>
2021-10-06 13:00:48 +0200

    gedicht

---
+++ PYTB1L2PoemPrinting/gedicht.py
@@ -0,0 +1,12 @@
+vers1 = " Mijn naam is Nick Kreuk."
+vers2 = "Ik lach mij altijd in een deuk\n"
+vers3 = "Ik ga nooit naar buiten."
+vers4 = "Door Covid gebruiken we nu veel vaccinatiespuiten\n"
+vers5 = "Ik consumeer erg wijnig literatuur."
+vers6 = "Mijn IQ is boven kamertemperatuur\n"
+vers7 = "Ik ben geboren in Duitsland."
+vers8 = "Maar mijn lievelings ontbijt is een croissant\n"
+vers9 = "Ik geloof niet in een elf."
+vers10 = "want ik geloof nieteens in mijzelf"
+
+print(vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers8,vers9,vers10 )
